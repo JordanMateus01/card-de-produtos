f6c5d053e7271d70fe2b051084aadfd98ec1c73b
Jordan Mateus <jordanmateus262@gmail.com>
2023-09-22 15:56:31 -0300

    Movendo todos os arquivos da master para main

---
+++ Assets/js/script.js
@@ -0,0 +1,10 @@
+function adicionarItem1() {
+    var n = 0;
+    var i = n + 1;
+    alert("Celular adicionado com sucesso ");
+};
+function adicionarItem2() {
+    var n = 0;
+    var i = n + 1;
+    alert("Notebook adicionado com sucesso ");
+};
